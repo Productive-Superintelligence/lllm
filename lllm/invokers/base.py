0fa939b27f89a4b8ce6d4d253ef14331ebc0f0f3
Cheng Jun-Yan <chengjunyan1@gmail.com>
2026-03-17 02:53:26 -0400

    1

--- lllm/invokers/base.py
+++ lllm/invokers/base.py
@@ -7,24 +7,6 @@ from lllm.core.const import APITypes, InvokeResult
 from lllm.core.dialog import Dialog
 
 
-class BaseInvoker(ABC):
-    @abstractmethod
-    def call(
-        self,
-        dialog: Dialog,
-        model: str,
-        model_args: Optional[Dict[str, Any]] = None,
-        parser_args: Optional[Dict[str, Any]] = None,
-        responder: str = 'assistant',
-        metadata: Optional[Dict[str, Any]] = None, # only for tracking additional information, such as frontend replay info
-        api_type: APITypes = APITypes.COMPLETION,
-        stream_handler: BaseStreamHandler = None,
-    ) -> InvokeResult:
-        """
-        Call the LLM and return the invocation result.
-        """
-        pass
-
 
 class BaseStreamHandler(ABC):
     @abstractmethod
@@ -41,5 +23,24 @@ class BaseStreamHandler(ABC):
         print(chunk_content) # or like link to your frontend display
         ```
         You can also log other things you are interested in in the object, but you need to handle the state yourself.
+        """
+        pass
+
+
+class BaseInvoker(ABC):
+    @abstractmethod
+    def call(
+        self,
+        dialog: Dialog,
+        model: str,
+        model_args: Optional[Dict[str, Any]] = None,
+        parser_args: Optional[Dict[str, Any]] = None,
+        responder: str = 'assistant',
+        metadata: Optional[Dict[str, Any]] = None, # only for tracking additional information, such as frontend replay info
+        api_type: APITypes = APITypes.COMPLETION,
+        stream_handler: BaseStreamHandler = None,
+    ) -> InvokeResult:
+        """
+        Call the LLM and return the invocation result.
         """
         pass
